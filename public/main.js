48cdf58578eee5c026928f8fccc90544b34641ee
Pratinav-Tewari <tpratinav@gmail.com>
2024-09-02 08:09:12 +0530

    nu

--- public/main.js
+++ public/main.js
@@ -1,41 +1,53 @@
 const socket = io()
-const userbase = document.getElementById("clients-total")
-const message = document.getElementById("message-container")
-const nam = document.getElementById("name-input")
-const form = document.getElementById("message-form")
-const input = document.getElementById("message-input")
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault()
-    sendMessage()
+const clientsTotal = document.getElementById('client-total')
+
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  sendMessage()
 })
+
 socket.on('clients-total', (data) => {
-    userbase.innerHTML = `User Base: ${data}`
+  clientsTotal.innerText = `User Base: ${data}`
 })
 
 function sendMessage() {
-    console.log(input.value)
-    const data = {
-        name: nam.value,
-        message: input.value,
-        dateTime: new Date()
-    }
-    socket.emit('message', data)
-    addMessage(true, data)
-    input.value = ''
+  if (messageInput.value === '') return
+  // console.log(messageInput.value)
+  const data = {
+    name: nameInput.value,
+    message: messageInput.value,
+    dateTime: new Date(),
+  }
+  socket.emit('message', data)
+  addMessage(true, data)
+  messageInput.value = ''
 }
 
 socket.on('chat-message', (data) => {
-    addMessage(false, data)
+  // console.log(data)
+  addMessage(false, data)
 })
 
-function addMessage(isOwnMessage, data){
-    const element = `<l1 class="${isOwnMessage ? "message-right" : "message-left"}">
-    <p class="message">
-        ${data.message}
-        <span>${moment(data.dateTime).fromNow()}</span>
-    </p>
-</l1>`
+function addMessage(isOwnMessage, data) {
+  const element = `
+      <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+          <p class="message">
+            ${data.message}
+            <span>${data.name} ● ${moment(data.dateTime).fromNow()}</span>
+          </p>
+        </li>
+        `
 
-message.innerHTML += element
+  messageContainer.innerHTML += element
+  scrollToBottom()
 }
+
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+  }
