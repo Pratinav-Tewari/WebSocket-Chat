4b3917942014354beaff2f231b3acc5184bbcedf
Pratinav-Tewari <tpratinav@gmail.com>
2024-09-04 10:10:33 +0530

    e

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`Chat server on port ${PORT}`))
 
 const io = require('socket.io')(server)
