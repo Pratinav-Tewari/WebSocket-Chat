b875467b3f4fce87f21dc7f8a2408f76621f258a
Pratinav-Tewari <tpratinav@gmail.com>
2024-09-04 11:34:53 +0530

    db

--- app.js
+++ app.js
@@ -1,33 +1,65 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const PORT = process.env.PORT || 4000
-const server = app.listen(PORT, () => console.log(`Chat server on port ${PORT}`))
+const express = require('express');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose(); 
+const app = express();
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => console.log(`Chat server on port ${PORT}`));
+const io = require('socket.io')(server);
 
-const io = require('socket.io')(server)
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')))
+let usersConnected = new Set();
 
-let usersConected = new Set()
+const db = new sqlite3.Database('./chat.db');  
 
-io.on('connection', conection)
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS chat (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, message TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)");
+});
 
-function conection(socket) {
-  console.log('Socket connected', socket.id)
-  usersConected.add(socket.id)
-  io.emit('user-base', usersConected.size)
+io.on('connection', connection);
 
-  socket.on('disconnect', () => {
-    console.log('Socket disconnected', socket.id)
-    usersConected.delete(socket.id)
-    io.emit('user-base', usersConected.size)
-  })
+function connection(socket) {
+    console.log('Socket connected', socket.id);
+    usersConnected.add(socket.id);
+    io.emit('user-base', usersConnected.size);
 
-  socket.on('message', (data) => {
-    socket.broadcast.emit('chat-message', data)
-  })
+    db.all("SELECT * FROM chat", [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        socket.emit('chat-history', rows);
+    });
 
-  socket.on('feedback', (data) => {
-    socket.broadcast.emit('feedback', data)
-  })
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected', socket.id);
+        usersConnected.delete(socket.id);
+        io.emit('user-base', usersConnected.size);
+    });
+
+    socket.on('message', (data) => {
+        const stmt = db.prepare("INSERT INTO chat (user, message) VALUES (?, ?)");
+        stmt.run(data.name, data.message, function(err) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            socket.broadcast.emit('chat-message', data);
+        });
+        stmt.finalize();
+    });
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data);
+    });
 }
+
+app.post('/clear-history', (req, res) => {
+    db.run(`DELETE FROM chat`, [], function(err) {
+      if (err) {
+        return 0;
+      }
+      io.emit('clear-history'); 
+      res.status(200).send({ success: true });
+    });
+  });
--- public/main.js
+++ public/main.js
@@ -1,88 +1,116 @@
-const socket = io()
-
-const clientsTotal = document.getElementById('user-base')
-
-const Container = document.getElementById('message-container')
-const namIn = document.getElementById('name-input')
-const Form = document.getElementById('message-form')
-const msgIn = document.getElementById('message-input')
-
-const Tone = new Audio('/notification-tone.mp3')
+const socket = io();
+const clientsTotal = document.getElementById('user-base');
+const Container = document.getElementById('message-container');
+const namIn = document.getElementById('name-input');
+const Form = document.getElementById('message-form');
+const msgIn = document.getElementById('message-input');
+const clearH = document.getElementById('clear-history');
+const Tone = new Audio('/notification-tone.mp3');
 
 Form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  send()
-})
+    e.preventDefault();
+    send();
+});
 
 socket.on('user-base', (data) => {
-  clientsTotal.innerText = `User Base: ${data}`
-})
+    clientsTotal.innerText = `User Base: ${data}`;
+});
 
 function send() {
-  if (msgIn.value === '') return
-  const data = {
-    name: namIn.value,
-    message: msgIn.value,
-    dateTime: new Date(),
-  }
-  socket.emit('message', data)
-  addMessage(true, data)
-  msgIn.value = ''
+    if (msgIn.value === '') return;
+    const data = {
+        name: namIn.value,
+        message: msgIn.value,
+        dateTime: new Date(),
+    };
+    socket.emit('message', data);
+    addMessage(true, data);
+    msgIn.value = '';
 }
 
 socket.on('chat-message', (data) => {
-  Tone.play()
-  addMessage(false, data)
-})
+    Tone.play();
+    addMessage(false, data);
+});
 
 function addMessage(isOwnMessage, data) {
-  clearF()
-  const element = `
+    clearF();
+    const element = `
       <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
           <p class="message">
             ${data.message}
             <span>${data.name} ● ${moment(data.dateTime).fromNow()}</span>
           </p>
         </li>
-        `
+        `;
 
-  Container.innerHTML += element
-  scrollToBottom()
+    Container.innerHTML += element;
+    scrollToBottom();
 }
 
 function scrollToBottom() {
-  Container.scrollTo(0, Container.scrollHeight)
+    Container.scrollTo(0, Container.scrollHeight);
 }
 
 msgIn.addEventListener('focus', (e) => {
-  socket.emit('feedback', {
-    feedback: `${namIn.value} is typing a message`,
-  })
-})
+    socket.emit('feedback', {
+        feedback: `${namIn.value} is typing a message`,
+    });
+});
 
 msgIn.addEventListener('keypress', (e) => {
-  socket.emit('feedback', {
-    feedback: `${namIn.value} is typing a message`,
-  })
-})
+    socket.emit('feedback', {
+        feedback: `${namIn.value} is typing a message`,
+    });
+});
+
 msgIn.addEventListener('blur', (e) => {
-  socket.emit('feedback', {
-    feedback: '',
-  })
-})
+    socket.emit('feedback', {
+        feedback: '',
+    });
+});
 
 socket.on('feedback', (data) => {
-  clearF()
-  const element = `
+    clearF();
+    const element = `
         <li class="message-feedback">
           <p class="feedback" id="feedback">${data.feedback}</p>
         </li>
-  `
-  Container.innerHTML += element
-})
+    `;
+    Container.innerHTML += element;
+});
 
 function clearF() {
-  document.querySelectorAll('li.message-feedback').forEach((element) => {
-    element.parentNode.removeChild(element)
-  })
+    document.querySelectorAll('li.message-feedback').forEach((element) => {
+        element.parentNode.removeChild(element);
+    });
 }
+
+socket.on('chat-history', (history) => {
+    history.forEach(msg => {
+        const data = {
+            name: msg.user,
+            message: msg.message,
+            dateTime: msg.timestamp,
+        };
+        addMessage(false, data);
+    });
+});
+
+clearH.addEventListener('click', () => {
+  fetch('/clear-history', { method: 'POST' })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        console.log('Chat history cleared');
+      } 
+      else {
+        console.error('Failed in clearing chat history');
+      }
+    })
+    .catch(error => console.error('Error:', error));
+});
+
+socket.on('clear-history', () => {
+  document.getElementById('message-container').innerHTML = ''; 
+});
